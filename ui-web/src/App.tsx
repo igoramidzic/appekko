2d864189066333960c4edc428f8422dde4bd4d3f
Igor Amidzic <amidzicigor46@gmail.com>
2020-10-04 22:37:25 -0400

    Add routeing

--- ui-web/src/App.tsx
+++ ui-web/src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.scss';
+import HomePage from './pages/home/home-page';
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <h4>
-      Hello world!
-    </h4>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ ui-web/src/pages/home/home-page.tsx
@@ -0,0 +1,13 @@
+import './home-page.scss';
+
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
